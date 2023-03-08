42dc6c7f3fadb43e5d300865915b27c2b2e1e8b7
root <root@Blackhole>
2023-03-08 12:05:22 +0700

    first update

---
+++ install_ssl.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+dir=$(pwd)
+crt_file=$(cat "$dir"/crt_file.crt)
+key_file=$(cat "$dir"/key_file.key)
+list_domain=$(cat list_domain.txt)
+crt=$(printf %s ''$crt_file'' | jq -sRr @uri)
+key=$(printf %s ''$key_file'' | jq -sRr @uri)
+
+while read -r line; do
+    # whmapi1 --output=jsonpretty \
+    #     installssl \
+    #     domain=''$domain'' \
+    #     crt=''$crt'' \
+    #     key=''$key''
+    echo $line
+done <<<"$list_domain"
