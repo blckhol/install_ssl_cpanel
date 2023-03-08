af4d04e3d441a7a887d0bc8d98c4a7bf0cc1947b
root <root@Blackhole>
2023-03-08 12:21:20 +0700

    fix variable

--- install_ssl.sh
+++ install_ssl.sh
@@ -4,13 +4,13 @@ dir=$(pwd)
 crt_file=$(cat "$dir"/crt_file.crt)
 key_file=$(cat "$dir"/key_file.key)
 list_domain=$(cat "$dir"/list_domain.txt)
-crt=$(printf %s ''$crt_file'' | jq -sRr @uri)
-key=$(printf %s ''$key_file'' | jq -sRr @uri)
+crt=$(printf %s "$crt_file" | jq -sRr @uri)
+key=$(printf %s "$key_file" | jq -sRr @uri)
 
-while read -r line; do
+while read -r domain; do
     whmapi1 --output=jsonpretty \
         installssl \
         domain=''$domain'' \
-        crt=''$crt'' \
-        key=''$key''
+        crt="$crt" \
+        key="$key"
 done <<<"$list_domain"
